f58c9eb58984139a8c8d6ce2826641b6c772174e
Vox <github@brisbois.dev>
2026-03-02 12:46:15 +0100

    Add files via upload

---
+++ SearchButton.tsx
@@ -0,0 +1,5 @@
+<button className="grid grid-cols-[1.25rem_1fr_1.25rem] items-center w-full h-14 rounded-full bg-pink-500 text-white px-5">
+  <Search className="h-5 w-5 justify-self-start" />
+  <span className="text-base font-semibold text-center truncate">Rechercher</span>
+  <span aria-hidden className="h-5 w-5" />
+</button>
